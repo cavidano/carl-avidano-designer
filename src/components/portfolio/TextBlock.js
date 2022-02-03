86ea261511b3a3f6d579bb84fdc412df6cd8e65f
Carl Avidano <cavidano@gmail.com>
2022-02-03 10:40:34 -0500

    Update TextBlock.js

--- src/components/portfolio/TextBlock.js
+++ src/components/portfolio/TextBlock.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TextBlock = ({ children }) => {
-  return <div className="container narrow margin-y-4">{children}</div>
+  return (
+    <div className="container narrow margin-y-4">
+      {children}
+    </div>
+  )
 }
 
 export default TextBlock;
