8bb3189565325432cad7308072a7ca370b6bf7a9
Carl Avidano <cavidano@gmail.com>
2022-02-03 11:29:50 -0500

    Update portfolioSingle.js

--- src/templates/portfolioSingle.js
+++ src/templates/portfolioSingle.js
@@ -26,13 +26,13 @@ const portfolioSingle = ({ data }) => {
   return (
     <Fragment>
 
-      <div className="project-intro theme-primary height-100vh"
+      <div className="project-intro theme-primary"
         style={{
             '--primary': `#FFCB62`,
             '--primary-text': `var(--dark)`,
         }}>
       <Header />
-      <div className="container wide text-align-center padding-2">
+      <div className="container medium text-align-center">
 
         <h1 class="margin-bottom-4">{title}</h1>
 
