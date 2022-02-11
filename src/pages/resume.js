888ce8b010193f40a33f55ebee0c4a8d53c17f20
Carl Avidano <cavidano@gmail.com>
2022-02-11 14:45:53 -0500

    before tightening resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -8,59 +8,71 @@ import Header from '../components/Header.js';
 
 const Resume = ({ data }) => {
 
-    const experience = data.allExperienceYaml.nodes;
+    const employment = data.allEmploymentYaml.nodes;
+
+    const jobList = employment.map((job, index) => {
+
+        let company = job.company;
+        let title = job.title;
+        let dates = job.dates;
+        let description = job.description
+
+        return (
+            <div className="border-top padding-y-3">
+                
+                <div key={`${job}_${index}`} className="grid grid--column-8--lg gap-3">
+
+                    <div className="grid__colspan-3">
+                        <p className="margin-bottom-2">
+                            <strong className="display-block">
+                                {company}
+                            </strong>
+                            <span className="display-block font-size-md">
+                                {title}
+                            </span>
+                        </p>
+
+                        <p className="font-size-sm opacity-70">
+                            {dates}
+                        </p>
+
+                    </div>
+
+                    <div className="grid__colspan-5">
+
+                    <MDXRenderer>
+                        {description.body}
+                    </MDXRenderer> 
+
+                    </div>
+
+
+                </div>
+            
+            </div>
+        );
+    });
     
     return (
         <Fragment>
             <Header />
 
-            <div className="container medium">
+            <div className="container narrow medium--lg padding-y-3">
 
-                <h2 className="h5">Employment</h2>
+                <h2 className="h6 text-transform-uppercase">Employment</h2>
 
-                {experience.map((job, index) => {
-
-                    let company = job.company;
-                    let title = job.title;
-                    let dates = job.dates;
-                    let description = job.description
-
-                    return (
-                        <div key={`${job}_${index}`} className="grid grid--column-2 border-top padding-y-3">
-
-                            <div>
-                                <p className="margin-bottom-2">
-                                    <strong className="display-block">
-                                        {company}
-                                    </strong>
-                                    <span className="display-block font-size-md">
-                                        {title}
-                                    </span>
-                                </p>
-
-                                <p className="font-size-sm opacity-70">
-                                    {dates}
-                                </p>
-                            </div>
-
-                            <MDXRenderer>
-                                {description.body}
-                            </MDXRenderer> 
-
-                        </div>
-                    );
-                })}
+                {jobList}
 
             </div>
         </Fragment>
-    )
+    );
 }
 
 export default Resume;
 
 export const query = graphql`
 query queryResume {
-    allExperienceYaml {
+    allEmploymentYaml {
       nodes {
         description {
           body
