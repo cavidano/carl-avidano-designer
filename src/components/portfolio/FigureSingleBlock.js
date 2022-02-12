ccf46a0fe2b0765a9cfae527e0b26dab748ee09f
Carl Avidano <cavidano@gmail.com>
2022-02-12 16:27:25 -0500

    adding a lightbox

--- src/components/portfolio/FigureSingleBlock.js
+++ src/components/portfolio/FigureSingleBlock.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const FigureSingleBlock = ({ children, ...props }) => {
 
     const { caption } = props;
     
     return (
-        <figure className="medium margin-y-5">
+        <Fragment>
+            <figure className="medium margin-y-5">
 
+                {children}
+            
+                {caption != null && (
+                    <figcaption className="container narrow font-size-md">
+                        <p><em>{caption}</em></p>
+                    </figcaption>
+                )}
+
+            </figure>
             {children}
-        
-            {caption != null && (
-                <figcaption className="container narrow font-size-md">
-                    <p><em>{caption}</em></p>
-                </figcaption>
-            )}
-
-        </figure>
+        </Fragment>
     );
 }
 
---
+++ src/components/portfolio/Lightbox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LightBox = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default LightBox;
