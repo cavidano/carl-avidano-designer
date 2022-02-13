112c6e4d3ca07d646e8e13fc77234bfaa0d6ef3e
Carl Avidano <cavidano@gmail.com>
2022-02-13 06:50:29 -0500

    cleaning up lightbox

--- src/components/portfolio/Lightbox.js
+++ src/components/portfolio/Lightbox.js
@@ -4,9 +4,9 @@ const LightBox = ({children, handleClose, id}) => {
 	return (
 		<div className="lightbox" id={id} onClick={handleClose}>
 
-			<div className="lightbox__actions">
+			<div className="lightbox__actions font-size-md">
 
-				<a class="button button--icon-only theme-canvas" href="#1" aria-label="Home">
+				<a class="button button--icon-only" href="#1" aria-label="Home">
 					<span class="icon icon-close" aria-hidden="true"></span>
 				</a>
 
