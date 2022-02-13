265645c5e95de0e3b821571d39865148cccc6455
Carl Avidano <cavidano@gmail.com>
2022-02-13 06:35:42 -0500

    cleaning up lightbox

--- src/components/portfolio/Lightbox.js
+++ src/components/portfolio/Lightbox.js
@@ -3,10 +3,17 @@ import React from 'react';
 const LightBox = ({children, handleClose, id}) => {
 	return (
 		<div className="lightbox" id={id} onClick={handleClose}>
+
+			<div className="lightbox__actions">
+
+				<a class="button button--icon-only theme-canvas" href="#1" aria-label="Home">
+					<span class="icon icon-close" aria-hidden="true"></span>
+				</a>
+
+			</div>
+
 			<div className="lightbox__container">
-
 				{children}
-
 			</div>
 		</div>
 	);
