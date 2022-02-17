7fd2752a427dc053e27fbf60bd5a9a698d2c812d
Carl Avidano <cavidano@gmail.com>
2022-02-17 13:21:56 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="copyright-container font-size-sm">
+    <footer className="font-size-sm">
         <p className='theme-canvas padding-1 border-radius'>
           &#169; <span className="copyright-year"></span>, Carl Avidano.
         </p>
